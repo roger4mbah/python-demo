a585e0e593f6f8ba3eb9dc9d3f4efff069373401
Roger Mbah <roger4mbah@gmail.com>
2024-08-17 23:42:49 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+sentinel = 100
+for i in range(1, sentinel + 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzFuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Fuzz")
+    else:
+        print(i)
